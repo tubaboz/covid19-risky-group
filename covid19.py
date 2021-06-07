84aa064d22eb49d02e91ff6c42d3c45a8b614960
tubasaf <77580305+tubasaf@users.noreply.github.com>
2021-06-07 23:38:19 +0200

    risky group of covid19

---
+++ covid19.py
@@ -0,0 +1,9 @@
+age = input("Are you a cigarette addict older than 75 years old? (Yes/No)").title().strip() == "Yes"
+chronic = input("Do you have a severe chronic disease? (Yes/No)").title().strip() == "Yes"
+immune = input("Is your immune system too weak? (Yes/No)").title().strip() == "Yes"
+if age or chronic or immune:
+    print("You are in risky group.")
+else:
+    print("You are not in risky group.")
+
+
